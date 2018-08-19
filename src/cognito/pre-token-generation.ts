cd060867820e3e885f5ebab9ac69ab9a2e0cf96e
Caleb Brewer <caleb@cazzer.io>
2018-08-19 16:25:44 -0700

    building token based on user ID

--- src/cognito/pre-token-generation.ts
+++ src/cognito/pre-token-generation.ts
@@ -1,8 +1,25 @@
 import { get } from 'lodash'
 import db from '../database'
 
-export default async function postUserConfirmation(event) {
-  console.log(event)
+export default async function preTokenGeneration(event) {
+  const userId = event.request.userAttributes.sub
 
-  return
+  console.log(`Getting group memberships for user ${userId}`)
+  const roles = db('user_group_membership')
+    .select('group_id')
+    .where('user_id', userId)
+
+  console.log(roles)
+
+  console.log('Building token roles')
+  return {
+    ...event,
+    response: {
+      claimOverrideDetails: {
+        claimsToAddOrOverride: {
+          roles: [userId]
+        }
+      }
+    }
+  }
 }
