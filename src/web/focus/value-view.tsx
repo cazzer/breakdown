1c69dbe4c8326d8faab11c503b393420c53d02cc
Caleb Brewer <caleb@cazzer.io>
2018-10-01 20:51:39 -0700

    making markdown show up better

--- src/web/focus/value-view.tsx
+++ src/web/focus/value-view.tsx
@@ -3,14 +3,23 @@ import Typography from '@material-ui/core/Typography'
 import Markdown from 'react-markdown'
 
 import { guessType } from './type-guesser'
+import { withStyles } from '@material-ui/core';
+
+const StyledMarkdown = withStyles(theme => ({
+  root: {
+    color: theme.palette.primary.contrastText
+  }
+}))(({ classes, source }) => (
+  <div className={classes.root}>
+    <Markdown source={source} />
+  </div>
+))
 
 export default ({ value }) => {
   const type = guessType(value)
   switch (type) {
     case 'markdown':
-      return (
-        <Markdown source={value} />
-      )
+      return <StyledMarkdown source={value} />
     default:
       return <Typography>{value}</Typography>
   }
