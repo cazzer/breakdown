6363104cc2640abad46a6adcbba43c6ac1bbff8b
Caleb Brewer <caleb@cazzer.io>
2019-07-15 23:14:29 -0700

    logging better information

--- src/dynamo-trigger/index.ts
+++ src/dynamo-trigger/index.ts
@@ -2,4 +2,8 @@ import epsagon from '../epsagon'
 
 export default epsagon.lambdaWrapper((event: any) => {
   console.log(event)
+  event.Records.forEach(record => {
+    console.log(record.dynamodb)
+    console.log(record.userIdentity)
+  })
 })
