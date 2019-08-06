8bd76ff9597e2e05a069e74f2c4f129f7708295a
Caleb Brewer <caleb@cazzer.io>
2019-08-05 22:49:59 -0700

    removing dynamo trigger

--- src/dynamo-trigger/index.ts
+++
@@ -1,9 +0,0 @@
-import epsagon from '../epsagon'
-
-export default epsagon.lambdaWrapper((event: any) => {
-  console.log(event)
-  event.Records.forEach(record => {
-    console.log(record.dynamodb)
-    console.log(record.userIdentity)
-  })
-})
