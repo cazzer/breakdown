99d80027775cd4de4d0d6305f782aa7d572a26a2
Caleb Brewer <caleb@cazzer.io>
2019-08-20 17:14:12 -0600

    removing epsagon from websocket handlers for now

--- src/websocket-gateway/connections.ts
+++ src/websocket-gateway/connections.ts
@@ -1,19 +1,19 @@
 import epsagon from '../epsagon'
 
-export const connect = epsagon.lambdaWrapper(async (event: any) => {
+export async function connect(event: any): Promise<object> {
   console.log(event)
 
   return {
     body: 'Connected',
     statusCode: 200
   }
-})
+}
 
-export const disconnect = epsagon.lambdaWrapper(async (event: any) => {
+export async function disconnect(event: any): Promise<object> {
   console.log(event)
 
   return {
     body: 'Disconnected',
     statusCode: 200
   }
-})
+}
