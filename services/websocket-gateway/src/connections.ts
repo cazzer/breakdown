3b5fc11fdc512b425a8e863f9d67508085ab165d
Caleb Brewer <caleb@cazzer.io>
2019-10-19 17:27:40 -0700

    fixing diconnect handler

--- services/websocket-gateway/src/connections.ts
+++ services/websocket-gateway/src/connections.ts
@@ -5,8 +5,7 @@ import epsagon from './epsagon'
 const DDBDocClient = new DynamoDB.DocumentClient({ apiVersion: '2012-10-08' })
 
 export const connect = epsagon.lambdaWrapper(async (event: any) => {
-  console.log(event)
-
+  console.log('Saving the connection')
   await DDBDocClient.put({
     Item: {
       connection_id: event.requestContext.connectionId,
@@ -15,6 +14,7 @@ export const connect = epsagon.lambdaWrapper(async (event: any) => {
     TableName: process.env.CONNECTIONS_TABLE_NAME
   }).promise()
 
+  console.log('Connection saved')
   return {
     body: 'Connected',
     statusCode: 200
@@ -22,15 +22,15 @@ export const connect = epsagon.lambdaWrapper(async (event: any) => {
 })
 
 export const disconnect = epsagon.lambdaWrapper(async (event: any) => {
-  console.log(event)
-
-  DDBDocClient.delete({
+  console.log('Deleting the connection...')
+  await DDBDocClient.delete({
     Key: {
       connection_id: event.requestContext.connectionId
     },
     TableName: process.env.CONNECTIONS_TABLE_NAME
-  })
+  }).promise()
 
+  console.log('Deleted connection')
   return {
     body: 'Disconnected',
     statusCode: 200
